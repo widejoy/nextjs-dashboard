d6a18bbb226ab320468b5d2259b2f7c6473ad0a0
widejoy <rogerantony40@gmail.com>
2025-01-27 17:20:15 +0530

    learn node

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const luistana = Lusitana({
+    subsets: ['latin'],
+    weight: '400'
+});
